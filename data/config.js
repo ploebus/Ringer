8305333679bd2a96ca2b10f3fc0c979374dd611f
johnny <johnny@johnny-Inspiron-6000.(none)>
2014-07-24 07:17:05 -0700

    change

---
+++ data/config.js
@@ -0,0 +1,48 @@
+
+var config ={
+	base:{
+			name:"Oakland",
+			file:"data/oakland_topo.json",
+			max:"",
+			field:"Sum_Probit"
+
+		},
+	rings:[
+		{
+			name:"Adult Medical",
+			file:"data/Ring1.geojson",
+			max:"",
+			field:"MC_adult"
+		},
+		{
+			name:"Children MediCal",
+			file:"data/R5temp.geojson",
+			max:"",
+			field:"FS_adult"
+		},
+		{
+			name:"Adult Snap Fresh",
+			file:"data/R2temp.geojson",
+			max:"",
+			field:"FS_adult"
+		},
+		{
+			name:"Children Snap Fresh",
+			file:"data/R6temp.geojson",
+			max:"",
+			field:"FS_child"
+		},
+		{
+			name:"Adult General Assistance",
+			file:"data/R3temp.geojson",
+			max:"",
+			field:"GA_adult"
+		},
+		{
+			name:"Adult CalWorks",
+			file:"data/R4temp.geojson",
+			max:"",
+			field:"CW_adult"
+		}
+		]
+};
